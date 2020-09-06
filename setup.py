a9fd3a3ab0347bb3044944bee1bfac006fd925e7
Dorian Jaminais-Grellier <azure@jaminais.fr>
2020-09-06 22:38:09 +0000

    release new version

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
+from configparser import ConfigParser
+from datetime import date
+
+import requests
 from setuptools import setup
+from urllib_ext.parse import urlparse
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open("README.md", "r") as fd:
+    long_description = fd.read()
+
+
+def get_dependencies():
+    pipfile = ConfigParser()
+    assert pipfile.read("Pipfile"), "Could not read Pipfile"
+    return list(pipfile["packages"])
+
+
+def get_next_version(project: str):
+    project_url = urlparse("https://pypi.org/project") / project
+    today = date.today()
+    version = f"{today:%Y}.{today:%m}.{today:%d}"
+    minor = 0
+    while requests.get(str(project_url / version)).status_code == 200:
+        minor += 1
+        version = f"{today:%Y}.{today:%m}.{today:%d}.{minor}"
+    return version
 
 
 setup(
     name="sharedvault",
-    version="1.0",
+    version=get_next_version("sharedvault"),
     author="Dorian Jaminais",
     author_email="sharedvault@jaminais.fr",
     description="SharedVault is a small application that allows you to define a "
@@ -15,7 +37,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nanassito/sharedvault",
-    py_modules=["sharedvault"],
+    packages=["sharedvault"],
     test_suite="test_all",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -23,5 +45,5 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=["cryptography", "sqlalchemy"],
+    install_requires=get_dependencies(),
 )
